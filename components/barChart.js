6674252c46c4669f6274bf5a0e0075fd5a204aea
Ali Irani <aliirani74@gmail.com>
2017-08-26 16:37:37 +0430

    chart view completed

--- components/barChart.js
+++ components/barChart.js
@@ -4,18 +4,7 @@ import { Bar } from 'vue-chartjs'
 export default Bar.extend({
   props: ['chartData', 'update', 'options'],
   mounted () {
-    // Overwriting base render method with actual data.
     this.renderChart(this.chartData, this.options)
-    /*    this.renderChart({
-
-      datasets: [
-        {
-          label: 'شاخص ها',
-          backgroundColor: '#f87979',
-          data: this.chartData
-        }
-      ]
-    }) */
   },
   watch: {
     update: function () {
